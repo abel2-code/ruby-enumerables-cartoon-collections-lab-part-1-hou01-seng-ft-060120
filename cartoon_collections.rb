a7e52cfd1c48316c25d0af65fb5e630b7eaafe7b
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-25 21:44:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def list_dwarves(array)
   (1..3).each do |num|
     puts num
-    
+
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
